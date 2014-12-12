4cf2bd797221440158ff0bcb4977468e1804d8b9
Alvaro Santisteban <alvarosantisteban@gmail.com>
2014-12-12 18:22:34 +0100

    Create table rows at runtime (number of columns and rows still hardcoded).

--- app/src/main/java/com/alvarosantisteban/moderacion15m/ModerationActivity.java
+++ app/src/main/java/com/alvarosantisteban/moderacion15m/ModerationActivity.java
@@ -2,10 +2,12 @@ package com.alvarosantisteban.moderacion15m;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.alvarosantisteban.moderacion15m.model.Participant;
 import com.alvarosantisteban.moderacion15m.util.Constants;
@@ -15,6 +17,9 @@ import java.util.List;
 
 public class ModerationActivity extends Activity {
 
+    private static final String TAG = "ModerationActivity";
+
+    TableLayout table_layout;
     Participant mCurrentParticipant;
     List<Participant> mParticipants = new ArrayList<Participant>();
 
@@ -23,8 +28,43 @@ public class ModerationActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moderation);
 
+        table_layout = (TableLayout) findViewById(R.id.participants_table);
+
         // Create participants
-        createParticipantsList();
+        //createParticipantsList();
+
+        buildTable(5, 5);
+    }
+
+    private void buildTable(int rows, int cols) {
+        Log.d(TAG, "buildTable");
+        // outer for loop
+        for (int i = 1; i <= rows; i++) {
+
+            TableRow row = new TableRow(this);
+            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            Log.d(TAG, "buildTable1");
+
+            // inner for loop
+            for (int j = 1; j <= cols; j++) {
+
+                TextView tv = new TextView(this);
+                Log.d(TAG, "buildTable2");
+                tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+                Log.d(TAG, "buildTable3");
+               // tv.setBackgroundResource(R.drawable.cell_shape);
+                tv.setPadding(5, 5, 5, 5);
+                tv.setText("R " + i + ", C" + j);
+
+                row.addView(tv);
+
+            }
+
+            table_layout.addView(row);
+
+        }
     }
 
     private void createParticipantsList() {
@@ -37,6 +77,7 @@ public class ModerationActivity extends Activity {
         return new Participant(String.valueOf(num), 0, num*2, num*1, true);
     }
 
+    /*
     public void startTimer(View v){
         String message;
         switch(v.getId()){
@@ -52,8 +93,9 @@ public class ModerationActivity extends Activity {
             default:
                 message = "Otro";
         }
+
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+    }  */
 
     ///////////////////////////////////////////////////////////
     // MENU
