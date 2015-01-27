162a1390a97e7f2e0dc31b8c56e3fbcd25c1984b
Alvaro Santisteban <alvarosantisteban@gmail.com>
2015-01-27 16:40:25 +0100

    Delete the unused menus.

--- app/src/main/java/com/alvarosantisteban/moderacion15m/MainActivity.java
+++ app/src/main/java/com/alvarosantisteban/moderacion15m/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -121,31 +119,5 @@ public class MainActivity extends Activity {
                 keyboard.showSoftInput(mEditTextColumns, 0);
             }
         }, 200);
-    }
-
-    ////////////////////////////////////////////////////////////////
-    // MENU RELATED
-    ////////////////////////////////////////////////////////////////
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
